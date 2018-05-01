13182a186d9806708dd7139b54dc7414fc966f08
davehein <davehein@swbell.net>
2018-04-30 19:43:50 -0500

    Removed abort.c and exit.c

--- lib/exit.c
+++
@@ -1,9 +0,0 @@
-void exit(int status)
-{
-    inline("cogid reg0");
-    inline("mov reg1, reg0");
-    inline("shl reg1, #2");
-    inline("add reg1, ##$7ff80");
-    inline("wrlong #0, reg1");
-    inline("cogstop reg0");
-}
