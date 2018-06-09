3a20df2f3466ba9d09c606ce1bf7b6971efa0727
davehein <davehein@swbell.net>
2018-06-09 18:09:05 -0500

    Removed the -t (two pass) option from s2pasm and now check for known register names

--- lib/putch.c
+++ lib/putch.c
@@ -4,7 +4,8 @@ void putch(int val)
     __asm__("        shl     r0, #1");
     __asm__("        mov     r1, #10");
     __asm__("        getct   r2");
-    __asm__("loop    shr     r0, #1 wc");
+    __asm__("loop");
+    __asm__("        shr     r0, #1 wc");
     __asm__("        drvc    #62");
     __asm__("        addct1  r2, ##80000000/115200");
     __asm__("        waitct1");
