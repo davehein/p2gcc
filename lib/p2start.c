22ff734a9b732d15967f5c6b29a87e35c60cd0ed
davehein <davehein@swbell.net>
2019-02-06 22:36:39 -0600

    Added argc & argv to p2start & call to main. Added declaration of __files[10] and init_files function. Added _get_ptrb function that sets _start_address.  Added patch_sys_config function.

--- lib/p2start.c
+++ lib/p2start.c
@@ -1,9 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
 #define p2clkfreq (*(int *)0x14)
 #define p2baudrate (*(int *)0x1c)
 
-int main(void);
+int main(int argc, char **argv);
 
+FILE __files[10] = {{0}};
 int p2bitcycles = 694;
+int _start_address; 
+
+static void init_files(void)
+{
+    int i;
+    __files[0]._flag = 0x100000;
+    __files[1]._flag = 0x100000;
+    __files[2]._flag = 0x100000;
+    for (i = 3; i < 10; i++) __files[i]._flag = 0;
+}
+
+static int __attribute__((noinline)) _get_ptrb(void)
+{
+    __asm__("mov r0, ptrb");
+}
 
 void configserial(int baudrate)
 {
@@ -16,8 +35,16 @@ void _setbaud(int baudrate)
     configserial(baudrate);
 }
 
-void p2start(void)
+void patch_sys_config(void)
 {
+    memcpy(&p2clkfreq, (char *)&p2clkfreq + _start_address, 3 * 4);
+}
+
+void p2start(int argc, char **argv)
+{
+    _start_address = _get_ptrb();
+    if (_start_address) patch_sys_config();
     configserial(p2baudrate);
-    main();
+    init_files();
+    main(argc, argv);
 }
