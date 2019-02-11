c4c867c2dc4c78aa384d25be5bc3e31355243880
davehein <davehein@swbell.net>
2019-02-10 22:22:05 -0600

    No longer need to type run to run a file.  Now check file to make sure it's an executable.

--- shell.c
+++ shell.c
@@ -29,25 +29,39 @@ void Run(int argc, char **argv)
 {
     FILE *infile;
     volatile int *ptr = 0;
+    int buffer[25];
 
+#if 0
     if (argc < 2)
     {
         fprintf(stdoutfile, "usage: run file [parms ...]\n");
         return;
     }
+#endif
 
-    if (!(infile = fopen(argv[1], "r")))
+    if (!(infile = fopen(argv[0], "r")))
     {
-        fprintf(stdoutfile, "Could not open %s\n", argv[1]);
+        printf("Invalid command\n");
+        Help();
+        //fprintf(stdoutfile, "Could not open %s\n", argv[1]);
         return;
     }
 
-    fread((void *)ptr, 1, 128 * 1024, infile);
-    fclose(infile);
-    sd_unmount();
+    fread(buffer, 1, 32, infile);
+    if (buffer[5] != 80000000 || buffer[6] != 19146744 || buffer[7] != 115200)
+    {
+        printf("%s is not an executable file\n", argv[1]);
+        fclose(infile);
+        return;
+    }
+    memcpy((void *)ptr, buffer, 32);
     patch_sys_config();
-    *(int *)(8 * 4) = argc - 1;
-    *(int *)(9 * 4) = (int)&argv[1];
+    fread((void *)ptr+32, 1, 128 * 1024 - 32, infile);
+    fclose(infile);
+    getcwd(buffer, 100);
+    sd_unmount();
+    *(int *)(8 * 4) = argc;
+    *(int *)(9 * 4) = (int)argv;
     coginit(1, 0, 0);
     DIRA = 0;
     DIRB = 0;
@@ -55,6 +69,7 @@ void Run(int argc, char **argv)
     DIRB = 0x40000000;
     //sd_mount(59, 60, 58, 61);
     sd_mount(58, 61, 59, 60);
+    chdir(buffer);
 }
 
 void Cd(int argc, char **argv)
@@ -460,8 +475,8 @@ int main(int argc, char **argv)
             Pwd(num, tokens);
         else if (!strcmp(tokens[0], "mkdir"))
             MakeDir(num, tokens);
-        else if (!strcmp(tokens[0], "run"))
-            Run(num, tokens);
+        //else if (!strcmp(tokens[0], "run"))
+            //Run(num, tokens);
         else if (!strcmp(tokens[0], "exit"))
         {
             printf("Exiting\n");
@@ -469,8 +484,9 @@ int main(int argc, char **argv)
         }
         else
         {
-            printf("Invalid command\n");
-            Help();
+            Run(num, tokens);
+            //printf("Invalid command\n");
+            //Help();
         }
         CloseRedirection();
     }
